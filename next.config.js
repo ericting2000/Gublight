a8bcdbd3769e0c030e32afa10bf26a2810215f57
ericting2000 <erictingting2000@gmail.com>
2022-03-15 02:38:06 +0800

    ⬆️[config]Add SVG support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
