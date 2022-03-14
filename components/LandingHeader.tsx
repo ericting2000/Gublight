adc6cf2a5d2e81af7ed3d87b40f99ca94e6a93d8
ericting2000 <erictingting2000@gmail.com>
2022-03-15 02:40:03 +0800

    💄[UI]Headers

--- components/LandingHeader.tsx
+++ components/LandingHeader.tsx
@@ -3,13 +3,13 @@ import Gublight from '../public/assets/icon/GubLight.svg';
 import Github from '../public/assets/icon/Github.svg';
 import Link from 'next/link';
 
-function Header() {
+function LandingHeader() {
   return (
-    <div className="web-header bg-[#161B22] w-full h-20 ">
-      <div className="container flex justify-between items-center my-0 mx-auto  h-20 ">
+    <div className="web-header bg-[#040D21] w-full h-20 ">
+      <div className="container flex justify-between items-center my-0 mx-auto  h-20 px-10">
         <Link href="/">
           <a className="pt-[6px]">
-            <Image src={Gublight} alt="GubLight Logo" />
+            <Image src={Gublight} alt="GubLight Logo" height={42} width={165} />
           </a>
         </Link>
 
@@ -17,11 +17,11 @@ function Header() {
           href="https://github.com/ericting2000/2022-Dcard-Web-Frontend-Intern-Homework"
           className="pt-[6px]"
         >
-          <Image src={Github} alt="Github Logo" />
+          <Image src={Github} alt="Github Logo" height={30} width={30} />
         </a>
       </div>
     </div>
   );
 }
 
-export default Header;
+export default LandingHeader;
