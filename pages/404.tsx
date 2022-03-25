0a84b7ced46ac87a07f9ff30cda0409d9daf05c5
ericting2000 <erictingting2000@gmail.com>
2022-03-25 18:05:11 +0800

    💄[UI]Custom 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,14 +8,22 @@ export default function ErrorPage() {
       <div>
         <Header />
       </div>
-      <div className="container flex  justify-center items-center mt-20 mx-auto px-10 pb-20 2xl:pb-32">
-        <div>
-          <Image
-            src={Gublight}
-            alt="GubLight Logo No Back"
-            width={200}
-            height={200}
-          />
+      <div className="container flex flex-col  justify-center items-center mt-44 mx-auto px-10 pb-20 2xl:pb-32">
+        <div className="flex justify-center items-center text-white text-3xl sm:text-6xl lg:text-[70pt] 2xl:text-[90pt]">
+          <p className="mr-1 2xl:mr-3">4</p>
+          <div className="w-[8%]">
+            <Image
+              src={Gublight}
+              alt="GubLight Logo No Back"
+              width={83}
+              height={103}
+            />
+          </div>
+          <p className="ml-1 2xl:ml-3">4</p>
+          <p className="ml-2 lg:ml-4 2xl:ml-5">Not Found</p>
+        </div>
+        <div className="text-white sm:text-3xl lg:text-5xl 2xl:text-6xl sm:mt-4 ">
+          <p>{`This is not the page you're looking for.`}</p>
         </div>
       </div>
     </div>
