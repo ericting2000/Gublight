be296b7ae1ba0618a41c579144401de04245f41c
ericting2000 <erictingting2000@gmail.com>
2022-03-30 13:24:55 +0800

    ✏️ [type]Fix typo

--- pages/500.tsx
+++ pages/500.tsx
@@ -11,7 +11,7 @@ export default function ErrorPage() {
       <div className="container flex flex-col  justify-center items-center mt-44 mx-auto px-10 pb-20 2xl:pb-32">
         <div className="flex-wrap flex justify-center items-center text-white text-3xl sm:text-6xl lg:text-[70pt] 2xl:text-[90pt]">
           <p className="mr-1 2xl:mr-3">500</p>
-          <p className="ml-2 lg:ml-4 2xl:ml-5">Internal Sever Error</p>
+          <p className="ml-2 lg:ml-4 2xl:ml-5">Internal Server Error</p>
         </div>
         <div className="text-white sm:text-3xl lg:text-5xl 2xl:text-6xl sm:mt-4 ">
           <p>{`Please contact the developer, thank you.`}</p>
