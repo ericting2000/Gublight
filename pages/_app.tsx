420d54d908d1401c52739fafc4a537b37e7e0d6c
ericting2000 <erictingting2000@gmail.com>
2022-04-03 01:52:06 +0800

    💫 [Animation]Add route change animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,34 @@
 import '../styles/globals.sass';
 import '../styles/color.sass';
 import type { AppProps } from 'next/app';
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import dynamic from 'next/dynamic';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const delay = 250; // in milliseconds
+    let timer: ReturnType<typeof setTimeout>;
+    const load = () => {
+      timer = setTimeout(function () {
+        NProgress.start();
+      }, delay);
+    };
+    const stop = () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+    Router.events.on('routeChangeStart', () => load());
+    Router.events.on('routeChangeComplete', () => stop());
+    Router.events.on('routeChangeError', () => stop());
+  }, []);
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
